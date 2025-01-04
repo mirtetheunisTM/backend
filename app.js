014eb807ada6d81ca52f3b080fb852a985d86975
Mirte Theunis <mirte.theunis@student.ucll.be>
2025-01-04 14:20:56 +0100

    change authentication

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/orders', passport.authenticate('jwt', { session: false }), ordersRouter);
+app.use('/api/v1/orders', ordersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api/v1/orders.js
+++ routes/api/v1/orders.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const ordersController = require('../../../controllers/api/v1/orders');
+const passport = require('../../../passport/passport');
 
-router.get('/', ordersController.getAll);
+router.get('/', passport.authenticate('jwt', { session: false }), ordersController.getAll);
 
 router.post('/', ordersController.create);
 
-router.get('/:id', ordersController.getOne);
+router.get('/:id', passport.authenticate('jwt', { session: false }), ordersController.getOne);
 
-router.put('/:id', ordersController.update);
+router.put('/:id', passport.authenticate('jwt', { session: false }), ordersController.update);
 
-router.delete('/:id', ordersController.destroy);
+router.delete('/:id', passport.authenticate('jwt', { session: false }), ordersController.destroy);
 
 module.exports = router;
